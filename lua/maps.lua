03b21aff7d04f7142aec3b889b38704ff70a1ad3
Thomas Steglich <info@thomas-steglich.de>
2024-06-14 07:54:43 +0200

    Packer install doku, ändere maps

--- lua/maps.lua
+++ lua/maps.lua
@@ -46,12 +46,6 @@ keymap("v", ">", ">gv", opts)
 -- toggle highlightsearch
 keymap('n', '<Leader>h', ':set hls!<cr>', opts)
 
-local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
-vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
-vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
-vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
-
 vim.keymap.set("n", "<space>tf", function()
 	require("telescope").extensions.file_browser.file_browser()
 end)
--- readme.md
+++ readme.md
@@ -58,6 +58,9 @@ q			close blame window
 
 ## Packer
 
+Damit Packer genutzt werden kann, muss es extra installiert werden.
+Die Installationsanweisung findet man im Github Repo unter https://github.com/wbthomason/packer.nvim
+
 -- You must run this or `PackerSync` whenever you make changes to your plugin configuration
 -- Regenerate compiled loader file
 :PackerCompile

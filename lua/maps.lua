b5214f78ed3d5d76f6b4ce47c2efe7fc997e554c
Thomas Steglich <info@thomas-steglich.de>
2025-05-15 09:21:33 +0200

    add keymaps for Obsidian commands

--- lua/maps.lua
+++ lua/maps.lua
@@ -44,6 +44,7 @@ keymap("v", ">", ">gv", opts)
 -- toggle highlightsearch
 keymap('n', '<Leader>h', ':set hls!<cr>', opts)
 
+-- Asciidoctor und Pdf Funktionen
 vim.keymap.set("n", "<leader>ar", function ()
 	local input_file = vim.fn.expand("%")
 	local output_file = input_file:gsub("%.adoc$", ".pdf")
@@ -57,3 +58,25 @@ vim.keymap.set("n", "<leader>ao", function ()
 	vim.cmd("!open -a Safari " .. output_file .. " &")
 end, { desc = "PDF mit Safari öffnen", noremap = true, silent = true})
 
+-- Obsidian keymap
+-- built in:
+-- gf in normal_mode = goto file under cursor
+vim.keymap.set("n", "<leader>od", function ()
+	vim.cmd("ObsidianDailies")
+end, { desc = "Opens dailies in telescope", noremap = true, silent = true })
+
+vim.keymap.set("n", "<leader>ot", function ()
+	vim.cmd("ObsidianToday")
+end, { desc = "Opens the today daily note", noremap = true, silent = true })
+
+vim.keymap.set("n", "<leader>oc", function ()
+	vim.cmd("ObsidianTOC")
+end, { desc = "Opens the table of content in a picker list", noremap = true, silent = true })
+
+vim.keymap.set("n", "<leader>oo", function ()
+	vim.cmd("ObsidianQuickSwitch")
+end, { desc = "switch to a note chosen in telescope", noremap = true, silent = true })
+
+vim.keymap.set("n", "<leader>os", function ()
+	vim.cmd("ObsidianSearch")
+end, { desc = "grep search in telescope", noremap = true, silent = true })

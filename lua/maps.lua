046e091bad79c690bbb8d499ea00ff363366fc6b
Thomas Steglich <info@thomas-steglich.de>
2025-05-22 22:18:02 +0200

    add gp.nvim and keyboard shortcuts for using openai and ChatGPT

--- lua/maps.lua
+++ lua/maps.lua
@@ -8,6 +8,15 @@
 
 local opts = { noremap = true, silent = true }
 
+local function keymapOptions(desc)
+    return {
+        noremap = true,
+        silent = true,
+        nowait = true,
+        desc = "GPT prompt " .. desc,
+    }
+end
+
 --Remap space as leader key
 vim.keymap.set("n", "<Space>", "", opts)
 vim.g.mapleader = " "
@@ -83,3 +92,38 @@ end, { desc = "grep search in telescope", noremap = true, silent = true })
 vim.keymap.set("n", "<leader>lf", function()
   vim.lsp.buf.format()
 end, { desc = "Format current buffer" })
+
+-- Gp OpenAI
+-- Chat Commands
+vim.keymap.set({"n", "i"}, "<C-g>c", "<cmd>GpChatNew<cr>", keymapOptions("New Chat"))
+vim.keymap.set({"n", "i"}, "<C-g>t", "<cmd>GpChatToggle<cr>", keymapOptions("Toggle Chat"))
+vim.keymap.set({"n", "i"}, "<C-g>f", "<cmd>GpChatFinder<cr>", keymapOptions("Chat Finder"))
+
+vim.keymap.set("v", "<C-g>c", ":<C-u>'<,'>GpChatNew<cr>", keymapOptions("Visual Chat New"))
+vim.keymap.set("v", "<C-g>p", ":<C-u>'<,'>GpChatPaste<cr>", keymapOptions("Visual Chat Paste"))
+vim.keymap.set("v", "<C-g>t", ":<C-u>'<,'>GpChatToggle<cr>", keymapOptions("Visual Toggle Chat"))
+
+vim.keymap.set({ "n", "i" }, "<C-g><C-v>", "<cmd>GpChatNew vsplit<cr>", keymapOptions("New Chat vsplit"))
+vim.keymap.set("v", "<C-g><C-v>", ":<C-u>'<,'>GpChatNew vsplit<cr>", keymapOptions("Visual Chat New vsplit"))
+
+-- Prompt Commands
+vim.keymap.set({"n", "i"}, "<C-g>r", "<cmd>GpRewrite<cr>", keymapOptions("Inline Rewrite"))
+vim.keymap.set({"n", "i"}, "<C-g>a", "<cmd>GpAppend<cr>", keymapOptions("Append (after)"))
+vim.keymap.set({"n", "i"}, "<C-g>b", "<cmd>GpPrepend<cr>", keymapOptions("Prepend (before)"))
+
+vim.keymap.set("v", "<C-g>r", ":<C-u>'<,'>GpRewrite<cr>", keymapOptions("Visual Rewrite"))
+vim.keymap.set("v", "<C-g>a", ":<C-u>'<,'>GpAppend<cr>", keymapOptions("Visual Append (after)"))
+vim.keymap.set("v", "<C-g>b", ":<C-u>'<,'>GpPrepend<cr>", keymapOptions("Visual Prepend (before)"))
+vim.keymap.set("v", "<C-g>i", ":<C-u>'<,'>GpImplement<cr>", keymapOptions("Implement selection"))
+
+vim.keymap.set({"n", "i"}, "<C-g>gp", "<cmd>GpPopup<cr>", keymapOptions("Popup"))
+
+vim.keymap.set("v", "<C-g>gp", ":<C-u>'<,'>GpPopup<cr>", keymapOptions("Visual Popup"))
+vim.keymap.set("v", "<C-g>ge", ":<C-u>'<,'>GpEnew<cr>", keymapOptions("Visual GpEnew"))
+vim.keymap.set("v", "<C-g>gn", ":<C-u>'<,'>GpNew<cr>", keymapOptions("Visual GpNew"))
+vim.keymap.set("v", "<C-g>gv", ":<C-u>'<,'>GpVnew<cr>", keymapOptions("Visual GpVnew"))
+
+vim.keymap.set({"n", "i"}, "<C-g>x", "<cmd>GpContext<cr>", keymapOptions("Toggle Context"))
+vim.keymap.set("v", "<C-g>x", ":<C-u>'<,'>GpContext<cr>", keymapOptions("Visual Toggle Context"))
+
+vim.keymap.set({"n", "i", "v", "x"}, "<C-g>s", "<cmd>GpStop<cr>", keymapOptions("Stop"))
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,54 +1,70 @@
 return {
-	{"nvim-lualine/lualine.nvim"},
-	{"nvim-lua/plenary.nvim"},
-	{"nvim-telescope/telescope.nvim"},
-	{"nvim-telescope/telescope-file-browser.nvim"},
-	{"windwp/nvim-autopairs"},
-	{"williamboman/nvim-lsp-installer"},
-	{"neovim/nvim-lspconfig"},
-	{"dinhhuy258/git.nvim"},
-	{"onsails/lspkind-nvim"},
-	{"L3MON4D3/LuaSnip"},
-	{"hrsh7th/cmp-buffer"},
-	{"hrsh7th/cmp-nvim-lsp"},
-	{"hrsh7th/nvim-cmp"},
-	{"kyazdani42/nvim-tree.lua"},
-	{"sindrets/diffview.nvim"},
-	{"tpope/vim-fugitive"},
+  { "nvim-lualine/lualine.nvim" },
+  { "nvim-lua/plenary.nvim" },
+  { "nvim-telescope/telescope.nvim" },
+  { "nvim-telescope/telescope-file-browser.nvim" },
+  { "windwp/nvim-autopairs" },
+  { "williamboman/nvim-lsp-installer" },
+  { "neovim/nvim-lspconfig" },
+  { "dinhhuy258/git.nvim" },
+  { "onsails/lspkind-nvim" },
+  { "L3MON4D3/LuaSnip" },
+  { "hrsh7th/cmp-buffer" },
+  { "hrsh7th/cmp-nvim-lsp" },
+  { "hrsh7th/nvim-cmp" },
+  { "kyazdani42/nvim-tree.lua" },
+  { "sindrets/diffview.nvim" },
+  { "tpope/vim-fugitive" },
   {
-      'brianhuster/live-preview.nvim',
-      dependencies = {
-          -- You can choose one of the following pickers
-          'nvim-telescope/telescope.nvim'
-      },
+    'brianhuster/live-preview.nvim',
+    dependencies = {
+      -- You can choose one of the following pickers
+      'nvim-telescope/telescope.nvim'
+    },
   },
-	{
-	  "epwalsh/obsidian.nvim",
-	  version = "*",  -- recommended, use latest release instead of latest commit
-	  lazy = true,
-	  ft = "markdown",
-	  -- Replace the above line with this if you only want to load obsidian.nvim for markdown files in your vault:
-	  -- event = {
-	  --   -- If you want to use the home shortcut '~' here you need to call 'vim.fn.expand'.
-	  --   -- E.g. "BufReadPre " .. vim.fn.expand "~" .. "/my-vault/*.md"
-	  --   -- refer to `:h file-pattern` for more examples
-	  --   "BufReadPre path/to/my-vault/*.md",
-	  --   "BufNewFile path/to/my-vault/*.md",
-	  -- },
-	  dependencies = {
-		"nvim-lua/plenary.nvim",
-	  },
-	  opts = {
-		workspaces = {
-		  {
-			name = "personal",
-			path = "~/vaults/personal",
-		  },
-		  {
-			name = "work",
-			path = "~/vaults/work",
-		  },
-		},
-	  },
-	}
+  {
+    "epwalsh/obsidian.nvim",
+    version = "*", -- recommended, use latest release instead of latest commit
+    lazy = true,
+    ft = "markdown",
+    -- Replace the above line with this if you only want to load obsidian.nvim for markdown files in your vault:
+    -- event = {
+    --   -- If you want to use the home shortcut '~' here you need to call 'vim.fn.expand'.
+    --   -- E.g. "BufReadPre " .. vim.fn.expand "~" .. "/my-vault/*.md"
+    --   -- refer to `:h file-pattern` for more examples
+    --   "BufReadPre path/to/my-vault/*.md",
+    --   "BufNewFile path/to/my-vault/*.md",
+    -- },
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+    },
+    opts = {
+      workspaces = {
+        {
+          name = "personal",
+          path = "~/vaults/personal",
+        },
+        {
+          name = "work",
+          path = "~/vaults/work",
+        },
+      },
+    },
+  },
+  {
+    "robitx/gp.nvim",
+    config = function()
+      local conf = {
+        providers = {
+          openai = {
+            endpoint = "https://api.openai.com/v1/chat/completions",
+            secret = os.getenv("OPENAI_API_KEY"),
+          },
+        }
+      }
+      require("gp").setup(conf)
+
+      -- Setup shortcuts here (see Usage > Shortcuts in the Documentation/Readme)
+    end,
+  }
 }

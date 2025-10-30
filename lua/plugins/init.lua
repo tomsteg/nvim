fdc4bec10e84389852cc121ca7bc9391706c8b0c
Thomas Steglich <info@thomas-steglich.de>
2025-10-30 23:07:13 +0100

    fix opencode

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -52,29 +52,20 @@ return {
     },
   },
   {
-    'NickvanDyke/opencode.nvim',
+    "NickvanDyke/opencode.nvim",
     dependencies = {
-      -- Recommended for better prompt input, and required to use opencode.nvim's embedded terminal — otherwise optional
-      { 'folke/snacks.nvim', opts = { input = { enabled = true } } },
+      { "folke/snacks.nvim", opts = { input = {}, picker = {}, terminal = {} } },
     },
-    ---@type opencode.Opts
-    opts = {
-      -- Your configuration, if any — see lua/opencode/config.lua
-    },
-    keys = {
-      -- Recommended keymaps
-      { '<leader>oA', function() require('opencode').ask() end, desc = 'Ask opencode', },
-      { '<leader>oa', function() require('opencode').ask('@cursor: ') end, desc = 'Ask opencode about this', mode = 'n', },
-      { '<leader>oa', function() require('opencode').ask('@selection: ') end, desc = 'Ask opencode about selection', mode = 'v', },
-      { '<leader>ot', function() require('opencode').toggle() end, desc = 'Toggle embedded opencode', },
-      { '<leader>on', function() require('opencode').command('session_new') end, desc = 'New session', },
-      { '<leader>oy', function() require('opencode').command('messages_copy') end, desc = 'Copy last message', },
-      { '<S-C-u>',    function() require('opencode').command('messages_half_page_up') end, desc = 'Scroll messages up', },
-      { '<S-C-d>',    function() require('opencode').command('messages_half_page_down') end, desc = 'Scroll messages down', },
-      { '<leader>op', function() require('opencode').select_prompt() end, desc = 'Select prompt', mode = { 'n', 'v', }, },
-      -- Example: keymap for custom prompt
-      { '<leader>oe', function() require('opencode').prompt("Explain @cursor and its context") end, desc = "Explain code near cursor", },
-    },
+    config = function()
+      vim.g.opencode_opts = { }
+
+      vim.o.autoread = true
+
+      vim.keymap.set({ "n", "x" }, "<leader>oa", function() require("opencode").ask("@this: ", { submit = true }) end, { desc = "Ask opencode" })
+      vim.keymap.set({ "n", "x" }, "<leader>ox", function() require("opencode").select() end, { desc = "Execute opencode action…" })
+      vim.keymap.set({ "n", "x" }, "<leader>op",    function() require("opencode").prompt("@this") end, { desc = "Add to opencode" })
+      vim.keymap.set("n", "<leader>ot",   function() require("opencode").toggle() end, { desc = "Toggle opencode" })
+    end,
   },
   {
     "akinsho/toggleterm.nvim",
@@ -82,18 +73,5 @@ return {
     config = function()
       require("toggleterm").setup()
     end,
-  },
-  {
-    "stevearc/conform.nvim",
-    opts = {
-      formatters_by_ft = {
-        markdown = { "prettier" },
-      },
-      format_on_save = {
-        lsp_fallback = true,
-        async = false,
-        timeout_ms = 5000,
-      },
-    },
   }
 }

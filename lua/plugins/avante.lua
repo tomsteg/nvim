389c405e4f7d4be10befc9487a636c95c694ceab
Thomas Steglich <info@thomas-steglich.de>
2025-11-18 23:22:48 +0100

    finetuning avante

--- lua/plugins/avante.lua
+++ lua/plugins/avante.lua
@@ -1,19 +1,31 @@
 return {
   "yetone/avante.nvim",
   build = "make",
+  mode = "legacy",
   event = "VeryLazy",
   dependencies = {
     "nvim-lua/plenary.nvim",
     "MunifTanjim/nui.nvim",
-    "nvim-tree/nvim-web-devicons",
     "stevearc/dressing.nvim",
+    "nvim-telescope/telescope.nvim", -- for file_selector provider telescope
+    {
+      -- Make sure to set this up properly if you have lazy=true
+      'MeanderingProgrammer/render-markdown.nvim',
+      opts = {
+        file_types = { "markdown", "Avante" },
+      },
+      ft = { "markdown", "Avante" },
+    },
   },
   opts = {
+    ask = {
+      auto_completion = false,
+    },
     provider = "openai",
     providers = {
       openai = {
         endpoint = "https://api.openai.com/v1",
-        model = "gpt-4.1-mini",
+        model = "gpt-5.1-codex-mini",
         extra_request_body = {
           temperature = 0.2,
         },

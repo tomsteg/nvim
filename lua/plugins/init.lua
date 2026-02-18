ad9fe5e8d8049560b57f0b817aab27980cc65e16
Thomas Steglich <info@thomas-steglich.de>
2026-02-18 23:11:49 +0100

    remove claude code plugin

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -51,21 +51,6 @@ return {
       })
     end,
   },
-  { "folke/snacks.nvim" },
-  {
-    "coder/claudecode.nvim",
-    dependencies = { "folke/snacks.nvim" },
-    config = true,
-    keys = {
-      { "<leader>ac", "<cmd>ClaudeCode<cr>", desc = "Toggle Claude" },
-      { "<leader>af", "<cmd>ClaudeCodeFocus<cr>", desc = "Focus Claude" },
-      { "<leader>ar", "<cmd>ClaudeCode --resume<cr>", desc = "Resume Claude" },
-      { "<leader>aC", "<cmd>ClaudeCode --continue<cr>", desc = "Continue Claude" },
-      { "<leader>am", "<cmd>ClaudeCodeSelectModel<cr>", desc = "Select model" },
-      { "<leader>ab", "<cmd>ClaudeCodeAdd %<cr>", desc = "Add current buffer" },
-      { "<leader>as", "<cmd>ClaudeCodeSend<cr>", mode = "v", desc = "Send to Claude" },
-    },
-  },
   {
     "epwalsh/obsidian.nvim",
     dependencies = {

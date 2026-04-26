9de71a6244e6b83b440d9ad5684ed3a073991b4f
Thomas Steglich <info@thomas-steglich.de>
2026-04-26 16:40:55 +0200

    add transparent backgroung

--- lua/base.lua
+++ lua/base.lua
@@ -42,4 +42,9 @@ vim.api.nvim_create_autocmd('FileType', {
   end,
 })
 
+-- Transparenter Hintergrund
+vim.api.nvim_set_hl(0, "Normal",      { bg = "none" })
+vim.api.nvim_set_hl(0, "NormalNC",    { bg = "none" })
+vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+vim.api.nvim_set_hl(0, "SignColumn",  { bg = "none" })
 
